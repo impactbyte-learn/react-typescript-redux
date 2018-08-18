5400246f17db52771789d3339b53deb0ff676676
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-08-18 12:47:22 +0700

    Add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import * as React from "react";
 import { Provider } from "react-redux";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import store from "./store";
 
+import About from "./components/About";
 import Counter from "./components/Counter";
 
 export default class App extends React.Component {
   public render() {
     return (
-      <Provider store={store}>
-        <div>
-          <h1>Hello World</h1>
-          <Counter />
-        </div>
-      </Provider>
+      <BrowserRouter>
+        <Provider store={store}>
+          <div>
+            <h1>Hello World</h1>
+            <Link to="/">Home</Link> | <Link to="/about">About</Link>
+            {/* <Switch> */}
+            <Route exact path="/" component={Counter} />
+            <Route path="/about" component={About} />
+            {/* </Switch> */}
+          </div>
+        </Provider>
+      </BrowserRouter>
     );
   }
 }
