d0150725b9e45400f26bdbf99df9b2aa55fb2010
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-08-22 18:57:05 +0700

    add enzyme testing

---
+++ __tests__/setup.ts
@@ -0,0 +1,4 @@
+import * as Enzyme from "enzyme";
+import * as ReactAdapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new ReactAdapter() });
--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,8 @@ module.exports = {
   transform: {
     "^.+\\.tsx?$": "ts-jest"
   },
+  snapshotSerializers: ["enzyme-to-json/serializer"],
+  setupFiles: ["./__tests__/setup.ts"],
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
 };
